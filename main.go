fc989dd2faea86fdfbc49032f38a081c4f4cbd24
Sean Pont <seanpont@gmail.com>
2014-10-02 05:40:18 -0700

    Ping -- doesn't work

--- main.go
+++ main.go
@@ -240,91 +240,36 @@ func udpClient(args []string) {
 
 // ===== PING ================================================================
 
-// func ping(args []string) {
-// 	// THIS FUNCTION IS RETURNING THE FOLLOWING ERROR:
-// 	// dial ip4:icmp 206.190.36.45: operation not permitted
-// 	checkArgs(args, 1, "ping <host>")
-// 	ipAddr, err := net.ResolveIPAddr("ip", args[0])
-// 	gobro.ExitOnError(err, "Resolution Error!")
-// 	ipConn, err := net.DialIP("ip4:icmp", ipAddr, ipAddr)
-// 	gobro.ExitOnError(err)
-// 	defer ipConn.Close()
-// 	var msg [8]byte
-// 	msg[0] = 8  //echo
-// 	msg[5] = 13 // identifier
-// 	msg[7] = 37 // sequence
-// 	check := checkSum(msg[0:8])
-// 	msg[2] = byte(check >> 8)
-// 	msg[3] = byte(check & 255)
-
-// 	_, err = ipConn.Write(msg[:])
-// 	gobro.ExitOnError(err)
-// 	ipConn.Read(msg[0:])
-// 	fmt.Println(msg)
-// }
-
-// func checkSum(msg []byte) uint16 {
-// 	sum := 0
-// 	for n := 1; n < len(msg)-1; n += 2 {
-// 		sum += int(msg[n])*256 + int(msg[n+1])
-// 	}
-// 	sum = sum>>16 + (sum & 0xffff)
-// 	sum += sum >> 16
-// 	return uint16(^sum)
-// }
-
 func ping(args []string) {
-	checkArgs(args, 1, "Usage: ping <host>")
-
-	addr, err := net.ResolveIPAddr("ip", args[0])
+	// THIS FUNCTION IS NOT WORKING
+	checkArgs(args, 1, "ping <host>")
+	ipAddr, err := net.ResolveIPAddr("ip", args[0])
+	gobro.ExitOnError(err, "Resolution Error!")
+	ipConn, err := net.DialIP("ip4:icmp", ipAddr, ipAddr)
 	gobro.ExitOnError(err)
-
-	conn, err := net.DialIP("ip4:icmp", addr, addr)
-	gobro.ExitOnError(err)
-
-	var msg [512]byte
-	msg[0] = 8  // echo
-	msg[1] = 0  // code 0
-	msg[2] = 0  // checksum, fix later
-	msg[3] = 0  // checksum, fix later
-	msg[4] = 0  // identifier[0]
-	msg[5] = 13 //identifier[1]
-	msg[6] = 0  // sequence[0]
-	msg[7] = 37 // sequence[1]
-	len := 8
-
-	check := checkSum(msg[0:len])
+	defer ipConn.Close()
+	var msg [8]byte
+	msg[0] = 8  //echo
+	msg[5] = 13 // identifier
+	msg[7] = 37 // sequence
+	check := checkSum(msg[0:8])
 	msg[2] = byte(check >> 8)
 	msg[3] = byte(check & 255)
 
-	_, err = conn.Write(msg[0:len])
+	_, err = ipConn.Write(msg[:])
 	gobro.ExitOnError(err)
-
-	_, err = conn.Read(msg[0:])
-	gobro.ExitOnError(err)
-
-	fmt.Println("Got response")
-	if msg[5] == 13 {
-		fmt.Println("identifier matches")
-	}
-	if msg[7] == 37 {
-		fmt.Println("Sequence matches")
-	}
-
-	os.Exit(0)
+	ipConn.Read(msg[0:])
+	fmt.Println(msg)
 }
 
 func checkSum(msg []byte) uint16 {
 	sum := 0
-
-	// assume even for now
 	for n := 1; n < len(msg)-1; n += 2 {
 		sum += int(msg[n])*256 + int(msg[n+1])
 	}
-	sum = (sum >> 16) + (sum & 0xffff)
-	sum += (sum >> 16)
-	var answer uint16 = uint16(^sum)
-	return answer
+	sum = sum>>16 + (sum & 0xffff)
+	sum += sum >> 16
+	return uint16(^sum)
 }
 
 // ===== HELPERS =============================================================
